6be6b2a471f386163fb6d4235e4d6eb35f3654dd
seno zacky <senozacky2006@gmail.com>
2024-12-17 22:33:48 +0700

    update contact page

--- components/contact.tsx
+++ components/contact.tsx
@@ -121,6 +121,29 @@ const Contact = () => {
                 <p>Instagram</p>
                </div>
                </a>
+                {/* Facebook */}
+          <a
+            href="https://www.facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Facebook"
+          >
+            <div className="flex gap-2 mt-5">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              className="text-[#549688] hover:text-gray-400 transition-colors"
+            >
+             <path d="M20.9,2H3.1A1.1,1.1,0,0,0,2,3.1V20.9A1.1,1.1,0,0,0,3.1,22h9.58V14.25h-2.6v-3h2.6V9a3.64,3.64,0,0,1,3.88-4,20.26,20.26,0,0,1,2.33.12v2.7H17.3c-1.26,0-1.5.6-1.5,1.47v1.93h3l-.39,3H15.8V22h5.1A1.1,1.1,0,0,0,22,20.9V3.1A1.1,1.1,0,0,0,20.9,2Z"
+             fill='currentColor'/>
+            </svg>
+              <p>Facebook</p>
+            </div>
+          </a>
+           
             </div>
           </div>
           
